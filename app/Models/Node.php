4d33f76500552daa852197a17369562300c4e3e0
Rocky Slavin <rocky.slavin@gmail.com>
2017-08-01 13:06:47 -0500

    added models

---
+++ app/Models/Node.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Node extends Model
+{
+    protected $fillable = ['name', 'is_root'];
+
+//    public function getSynonyms
+}
